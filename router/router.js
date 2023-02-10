bf5cf7d6b73762fe52154be30179fde6981572fd
sutan mufti <sutan@ambersideenergy.com>
2023-02-10 16:31:34 +0000

    router: handling the webhook

--- router/router.js
+++ router/router.js
@@ -1,6 +1,7 @@
 import {Router} from "express";
-import { getTasks, createTask, editTask, getSingleTask } from "../modules/functions.js";
+import { getTasks, createTask, editTask, getSingleTask, handleWebhook, updateCustom } from "../modules/functions.js";
 import * as dotenv from 'dotenv'
+import { routerConfig } from "./router.config.js";
 dotenv.config()
 
 export const router = Router()
@@ -11,12 +12,12 @@ router.get("/",(req,res)=>{
 
 
 router.get("/clickup", async (req,res)=>{
-    return res.status(200).send({message: await getTasks({listId: 900500459390, apiKey: process.env.APIKEY})})
+    return res.status(200).send({message: await getTasks({listId: routerConfig.listId, apiKey: process.env.APIKEY})})
 })
 
 
 router.get("/clickup/create", async (req,res)=>{
-    return res.status(200).send({message: await createTask({listId: 900500459390, apiKey: process.env.APIKEY})})
+    return res.status(200).send({message: await createTask({listId: routerConfig.listId, apiKey: process.env.APIKEY})})
 })
 
 router.get("/clickup/edit/:taskId", async (req,res)=>{
@@ -32,5 +33,32 @@ router.get("/clickup/get/:taskId", async (req,res)=>{
 })
 
 router.get("/router",(req,res)=>{
-    return res.status(200).send({message: "hello from router!"})
+    return res.status(200).send({status:200, message: "hello from router!"})
+})
+
+
+router.get("/clickup/webhook/", async (req,res)=>{
+    return res.status(200).send({status:200, message: "clickup webhook"})
+})
+
+router.get("/clickup/webhook/:taskId/edit", async (req,res)=>{
+    const data = await updateCustom({taskId: req.params.taskId, apiKey: process.env.APIKEY, fieldId: routerConfig.affectedField})
+    return res.status(200).send({status:200, message: data})
+})
+
+
+router.post("/clickup/webhook/:taskId/edit", async (req,res)=>{
+    const data = await updateCustom({taskId: req.params.taskId, apiKey: process.env.APIKEY, fieldId: routerConfig.affectedField})
+    return res.status(200).send({status:200, message: data})
+})
+
+router.get("/clickup/webhook/:taskId", async (req,res)=>{
+    console.log("sending to handleWebhook")
+    const result = await handleWebhook({taskId: req.params.taskId, apiKey: process.env.APIKEY})
+    console.log("receiving data from handleWebhook")
+
+    const {value } = result.custom_fields.filter((d)=>{return d.id == routerConfig.customField})[0]
+    const relatedTaskIds = value.map(x => x.id)
+    // return res.status(200).send(relatedTaskIds)
+    return res.status(200).send(result)
 })
